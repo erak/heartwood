7501fb94ba09c1b3a86dfbb060eb51ee35859bf8
Erik Kundt <erik@zirkular.io>
2023-07-05 11:39:44 +0200

    tui: Implement form input w/ tui-realm-textarea

--- radicle-tui/src/ui/widget/common/container.rs
+++ radicle-tui/src/ui/widget/common/container.rs
@@ -249,7 +249,7 @@ impl WidgetComponent for Container {
     }
 
     fn state(&self) -> State {
-        State::None
+        self.component.state()
     }
 
     fn perform(&mut self, _properties: &Props, cmd: Cmd) -> CmdResult {
