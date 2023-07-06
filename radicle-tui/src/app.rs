197605ba1bef23416ab55494909688a2494038a2
Erik Kundt <erik@zirkular.io>
2023-07-06 16:36:08 +0200

    tui: Add message, warning and error popup

--- radicle-tui/src/app.rs
+++ radicle-tui/src/app.rs
@@ -9,6 +9,7 @@ use radicle::cob::patch::PatchId;
 use radicle::identity::{Id, Project};
 use radicle::profile::Profile;
 
+use radicle_tui::ui::widget;
 use tuirealm::application::PollStrategy;
 use tuirealm::{Application, Frame, NoUserEvent};
 
@@ -50,6 +51,7 @@ pub enum Cid {
     Issue(IssueCid),
     Patch(PatchCid),
     GlobalListener,
+    Popup,
 }
 
 /// Messages handled by this application.
@@ -71,11 +73,20 @@ pub enum PatchMessage {
 }
 
 #[derive(Debug, Eq, PartialEq)]
+pub enum PopupMessage {
+    Info(String),
+    Warning(String),
+    Error(String),
+    Hide,
+}
+
+#[derive(Debug, Eq, PartialEq)]
 pub enum Message {
     Home(HomeMessage),
     Issue(IssueMessage),
     Patch(PatchMessage),
     NavigationChanged(u16),
+    Popup(PopupMessage),
     Tick,
     Quit,
 }
@@ -150,6 +161,52 @@ impl App {
             ))
         }
     }
+
+    fn show_info_popup(
+        &mut self,
+        app: &mut Application<Cid, Message, NoUserEvent>,
+        theme: &Theme,
+        message: &str,
+    ) -> Result<()> {
+        let popup = widget::common::info(theme, message);
+        app.remount(Cid::Popup, popup.to_boxed(), vec![])?;
+        app.active(&Cid::Popup)?;
+
+        Ok(())
+    }
+
+    fn show_warning_popup(
+        &mut self,
+        app: &mut Application<Cid, Message, NoUserEvent>,
+        theme: &Theme,
+        message: &str,
+    ) -> Result<()> {
+        let popup = widget::common::warning(theme, message);
+        app.remount(Cid::Popup, popup.to_boxed(), vec![])?;
+        app.active(&Cid::Popup)?;
+
+        Ok(())
+    }
+
+    fn show_error_popup(
+        &mut self,
+        app: &mut Application<Cid, Message, NoUserEvent>,
+        theme: &Theme,
+        message: &str,
+    ) -> Result<()> {
+        let popup = widget::common::error(theme, message);
+        app.remount(Cid::Popup, popup.to_boxed(), vec![])?;
+        app.active(&Cid::Popup)?;
+
+        Ok(())
+    }
+
+    fn hide_popup(&mut self, app: &mut Application<Cid, Message, NoUserEvent>) -> Result<()> {
+        app.blur()?;
+        app.umount(&Cid::Popup)?;
+
+        Ok(())
+    }
 }
 
 impl Tui<Cid, Message> for App {
@@ -166,6 +223,10 @@ impl Tui<Cid, Message> for App {
     fn view(&mut self, app: &mut Application<Cid, Message, NoUserEvent>, frame: &mut Frame) {
         if let Ok(page) = self.pages.peek_mut() {
             page.view(app, frame);
+        }
+
+        if app.mounted(&Cid::Popup) {
+            app.view(&Cid::Popup, frame, frame.size());
         }
     }
 
@@ -186,6 +247,18 @@ impl Tui<Cid, Message> for App {
                         }
                         Message::Patch(PatchMessage::Leave) => {
                             self.pages.pop(app)?;
+                        }
+                        Message::Popup(PopupMessage::Info(info)) => {
+                            self.show_info_popup(app, &theme, &info)?;
+                        }
+                        Message::Popup(PopupMessage::Warning(warning)) => {
+                            self.show_warning_popup(app, &theme, &warning)?;
+                        }
+                        Message::Popup(PopupMessage::Error(error)) => {
+                            self.show_error_popup(app, &theme, &error)?;
+                        }
+                        Message::Popup(PopupMessage::Hide) => {
+                            self.hide_popup(app)?;
                         }
                         Message::Quit => self.quit = true,
                         _ => {
